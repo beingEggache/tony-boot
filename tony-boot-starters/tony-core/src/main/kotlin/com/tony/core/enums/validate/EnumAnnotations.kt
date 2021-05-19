f8fd424a9fd091d4b460ef535d50e787227a7772
7ony <beingeggache@foxmail.com>
2021-05-19 11:02:35 +0800

    Add:enum validate

---
+++ tony-boot-starters/tony-core/src/main/kotlin/com/tony/core/enums/validate/EnumAnnotations.kt
@@ -0,0 +1,63 @@
+/**
+ *
+ * @author tangli
+ * @since 2021-05-19 10:58
+ */
+package com.tony.core.enums.validate
+
+import javax.validation.Constraint
+import javax.validation.Payload
+import kotlin.reflect.KClass
+
+@MustBeDocumented
+@Constraint(validatedBy = [SimpleEnumValidator::class])
+@Target(
+    AnnotationTarget.FUNCTION,
+    AnnotationTarget.PROPERTY_GETTER,
+    AnnotationTarget.PROPERTY_SETTER,
+    AnnotationTarget.FIELD,
+    AnnotationTarget.VALUE_PARAMETER
+)
+@kotlin.annotation.Retention(AnnotationRetention.RUNTIME)
+annotation class SimpleEnum(
+    vararg val enums: String,
+    val message: String = "非法参数",
+    val groups: Array<KClass<*>> = [],
+    val payload: Array<KClass<out Payload>> = []
+)
+
+@MustBeDocumented
+@Constraint(validatedBy = [SimpleIntEnumValidator::class])
+@Target(
+    AnnotationTarget.FUNCTION,
+    AnnotationTarget.PROPERTY_GETTER,
+    AnnotationTarget.PROPERTY_SETTER,
+    AnnotationTarget.FIELD,
+    AnnotationTarget.VALUE_PARAMETER
+)
+@kotlin.annotation.Retention(AnnotationRetention.RUNTIME)
+annotation class SimpleIntEnum(
+    vararg val enums: Int,
+    val message: String = "非法参数",
+    val required: Boolean = false,
+    val groups: Array<KClass<*>> = [],
+    val payload: Array<KClass<out Payload>> = []
+)
+
+@MustBeDocumented
+@Constraint(validatedBy = [SimpleIntEnumValidator::class])
+@Target(
+    AnnotationTarget.FUNCTION,
+    AnnotationTarget.PROPERTY_GETTER,
+    AnnotationTarget.PROPERTY_SETTER,
+    AnnotationTarget.FIELD,
+    AnnotationTarget.VALUE_PARAMETER
+)
+@kotlin.annotation.Retention(AnnotationRetention.RUNTIME)
+annotation class SimpleStringEnum(
+    vararg val enums: String,
+    val message: String = "非法参数",
+    val required: Boolean = false,
+    val groups: Array<KClass<*>> = [],
+    val payload: Array<KClass<out Payload>> = []
+)
