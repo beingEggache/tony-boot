83bc0b0e4e972ee40bedba2ab816e9c25998130e
7ony <beingeggache@foxmail.com>
2021-10-22 17:54:47 +0800

    update:deps update

--- tony-micro-service-demo/buildSrc/build.gradle.kts
+++ tony-micro-service-demo/buildSrc/build.gradle.kts
@@ -30,5 +30,5 @@ tasks.withType<KotlinCompile>().configureEach {
 
 dependencies {
     implementation("com.palantir.gradle.docker:gradle-docker:0.29.0")
-    implementation("org.springframework.boot:spring-boot-gradle-plugin:2.5.5")
+    implementation("org.springframework.boot:spring-boot-gradle-plugin:2.5.6")
 }
