fae9bedfe24f49303ea0d72f959f21f2a9c14a48
tony <beingeggache@foxmail.com>
2021-12-19 22:23:27 +0800

    polish

--- tony-boot-starters/tony-feign/src/test/kotlin/com/tony/openfeign/test/OpenFeignTest.kt
+++ tony-boot-starters/tony-feign/src/test/kotlin/com/tony/openfeign/test/OpenFeignTest.kt
@@ -7,7 +7,6 @@ import com.tony.feign.interceptor.ByHeaderRequestProcessor
 import com.tony.feign.jsonNode
 import com.tony.feign.sortRequestBody
 import com.tony.utils.getLogger
-import com.tony.utils.println
 import com.tony.utils.toJsonString
 import com.tony.utils.toString
 import okhttp3.Request
@@ -19,16 +18,8 @@ import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
-import java.io.BufferedInputStream
-import java.io.BufferedReader
-import java.io.BufferedWriter
-import java.io.InputStreamReader
-import java.io.OutputStreamWriter
-import java.net.InetSocketAddress
-import java.net.Socket
 import java.time.LocalDateTime
 import javax.annotation.Resource
-import kotlin.streams.toList
 
 @SpringBootTest(classes = [OpenFeignApp::class])
 class OpenFeignTest {
@@ -50,60 +41,6 @@ class OpenFeignTest {
             throw RuntimeException("error")
         }
         logger.info(result.toJsonString())
-    }
-
-    @Test
-    fun test2() {
-        val socket = Socket()
-        val address = InetSocketAddress("localhost", 8080)
-        socket.connect(address)
-        val httpRequest = """|POST /test-url-post HTTP/1.1
-                             |Accept: */*
-                             |Content-Type: application/x-www-form-urlencoded
-                             |Content-Length: 21
-                             |Host: localhost:8080
-                             |Connection: Keep-Alive
-                             |Accept-Encoding: gzip
-                             |User-Agent: okhttp/4.9.1
-                             |
-                             |number=123&string=432""".trimMargin()
-        val httpRequest1 = """|POST /test-url-post HTTP/1.1
-                            |Host: localhost:8080
-                            |Connection: keep-alive
-                            |Content-Length: 21
-                            |Accept: */*
-                            |Content-Type: application/x-www-form-urlencoded; charset=UTF-8
-                            |sec-ch-ua-platform: "Windows"
-                            |Origin: http://localhost:8080
-                            |Sec-Fetch-Site: same-origin
-                            |Sec-Fetch-Mode: cors
-                            |Sec-Fetch-Dest: empty
-                            |Referer: http://localhost:8080/doc.html
-                            |Accept-Encoding: gzip, deflate, br
-                            |Accept-Language: zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6
-                              |
-                              |number=123&string=432""".trimMargin()
-        socket.use { s ->
-            s.getOutputStream().use { outputStream ->
-                OutputStreamWriter(outputStream).use { osw ->
-                    BufferedWriter(osw).use { bw ->
-                        bw.write(
-                            httpRequest
-                        )
-                        bw.flush()
-                    }
-                    s.getInputStream().use { inputStream ->
-                        BufferedInputStream(inputStream).use { bi ->
-                            InputStreamReader(bi).use { isr ->
-                                BufferedReader(isr).use { br ->
-                                    br.lines().use { it.toList().joinToString("\n") }.println()
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
     }
 }
 
