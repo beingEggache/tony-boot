38fc84e7340ae94240790e6450748c4d5056b147
tony <beingeggache@foxmail.com>
2023-07-12 20:14:11 +0800

    update: anno utils

--- tony-boot-starters/tony-core/src/main/kotlin/com/tony/utils/AnnotationUtils.kt
+++ tony-boot-starters/tony-core/src/main/kotlin/com/tony/utils/AnnotationUtils.kt
@@ -20,17 +20,13 @@ import org.springframework.core.annotation.AnnotationUtils
  * @author tangli
  * @since 2023/7/12 10:29
  */
-public fun <A : Annotation> Field.selfOrGetterOrSetterHasAnnotation(annotationType: Class<A>): Boolean {
-    val fieldHasAnno = AnnotationUtils.findAnnotation(this, annotationType) != null
-    if (fieldHasAnno) {
-        return true
-    }
-    val getter = getter()
-    if (getter?.hasAnnotation(annotationType) == true) {
-        return true
-    }
-    return setter()?.hasAnnotation(annotationType) == true
-}
+public fun <A : Annotation> Field.selfOrGetterOrSetterHasAnnotation(annotationType: Class<A>): Boolean =
+    annotationFromSelfOrGetterOrSetter(annotationType) != null
+
+public fun <A : Annotation> Field.annotationFromSelfOrGetterOrSetter(annotationType: Class<A>): A? =
+    AnnotationUtils.findAnnotation(this, annotationType)
+        ?: getter()?.annotation(annotationType)
+        ?: setter()?.annotation(annotationType)
 
 public fun <A : Annotation> Method.hasAnnotation(annotationType: Class<A>): Boolean =
     AnnotationUtils.findAnnotation(this, annotationType) != null
@@ -40,20 +36,6 @@ public fun <A : Annotation> Class<*>.hasAnnotation(annotationType: Class<A>): Bo
 
 public fun <A : Annotation> AnnotatedElement.hasAnnotation(annotationType: Class<A>): Boolean =
     AnnotationUtils.findAnnotation(this, annotationType) != null
-
-public fun <A : Annotation> Field.annotationFromSelfOrGetterOrSetter(annotationType: Class<A>): A? {
-    val fieldHasAnno = AnnotationUtils.findAnnotation(this, annotationType)
-    if (fieldHasAnno != null) {
-        return fieldHasAnno
-    }
-
-    val getter = getter()
-    val getterAnnotation = getter?.annotation(annotationType)
-    if (getterAnnotation != null) {
-        return getterAnnotation
-    }
-    return setter()?.annotation(annotationType)
-}
 
 public fun <A : Annotation> Method.annotation(annotationType: Class<A>): A? =
     AnnotationUtils.findAnnotation(this, annotationType)
