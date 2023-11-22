0d9991ffdfa81b46536a5f95895e7cc07c30fdbd
tangli <aaa@a.com>
2023-11-22 16:30:24 +0800

    polish

--- tony-boot-starters/tony-feign/src/test/kotlin/com/tony/test/feign/module/unwrap/client/OpenFeignTestUnwrapClient.kt
+++
@@ -1,7 +0,0 @@
-package com.tony.test.feign.module.unwrap.client
-
-import com.tony.test.feign.module.unwrap.api.OpenFeignUnwrapTestApi
-import org.springframework.cloud.openfeign.FeignClient
-
-@FeignClient(name = "openFeignTestUnwrapClient", url = "http://localhost:9090")
-interface OpenFeignTestUnwrapClient : OpenFeignUnwrapTestApi
