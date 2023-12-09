a3062b8c50f4e67746f0bb6822d27c663828765b
tangli <aaa@a.com>
2023-12-09 16:54:56 +0800

    polish

--- tony-monolithic-demo/tony-service/src/main/kotlin/com/tony/db/config/DbConfig.kt
+++ tony-monolithic-demo/tony-service/src/main/kotlin/com/tony/db/config/DbConfig.kt
@@ -6,16 +6,12 @@ import com.baomidou.mybatisplus.core.incrementer.IdentifierGenerator
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor
 import com.tony.ApiSession
-import com.tony.db.dao.UserDao
-import com.tony.db.po.User
 import com.tony.id.IdGenerator
 import com.tony.mybatis.DefaultMetaObjectHandler
-import jakarta.annotation.Resource
 import org.mybatis.spring.annotation.MapperScan
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Lazy
 import org.springframework.transaction.annotation.EnableTransactionManagement
 
 @Configuration
@@ -43,18 +39,4 @@ class DbConfig {
 
 class MonoApiMetaObjectHandler(
     apiSession: ApiSession,
-) : DefaultMetaObjectHandler(apiSession) {
-    @Lazy
-    @Resource
-    private lateinit var userDao: UserDao
-    override val userName: String?
-        get() =
-            userDao
-                .ktQuery()
-                .select(User::realName)
-                .eq(User::userId, userId)
-                .oneObj<String>()
-
-    override val tenantId: Long
-        get() = 0L
-}
+) : DefaultMetaObjectHandler(apiSession)
