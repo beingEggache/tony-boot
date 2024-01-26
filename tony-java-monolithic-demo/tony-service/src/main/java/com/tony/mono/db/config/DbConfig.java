76936f86fb4833308f174cf865e6242b2609a943
tangli <aaa@a.com>
2024-01-26 10:44:20 +0800

    polish

--- tony-java-monolithic-demo/tony-service/src/main/java/com/tony/mono/db/config/DbConfig.java
+++ tony-java-monolithic-demo/tony-service/src/main/java/com/tony/mono/db/config/DbConfig.java
@@ -5,15 +5,12 @@ import com.baomidou.mybatisplus.core.incrementer.IdentifierGenerator;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import com.tony.id.IdGenerator;
-import com.tony.misc.YamlPropertySourceFactory;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @ComponentScan(basePackages = "com.tony.mono.db")
-@PropertySource(value = "classpath:mybatis-plus.config.yml", factory = YamlPropertySourceFactory.class)
 @EnableTransactionManagement(proxyTargetClass = true)
 @MapperScan("com.tony.mono.db.dao")
 public class DbConfig {
