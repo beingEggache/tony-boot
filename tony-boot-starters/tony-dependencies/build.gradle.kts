e4686e66c4d52c139204dd5c42a79c861b24581e
tangli <aaa@a.com>
2024-03-13 16:49:54 +0800

    polish

--- tony-boot-starters/tony-dependencies/build.gradle.kts
+++ tony-boot-starters/tony-dependencies/build.gradle.kts
@@ -13,8 +13,10 @@ dependencies {
         }
     }
     add("api", platform(tonyLibs.kotlinBom))
-    add("api", platform(tonyLibs.springCloudDependencies))
+    add("api", platform(tonyLibs.springBom))
     add("api", platform(tonyLibs.reactorBom))
     add("api", platform(tonyLibs.nettyBom))
     add("api", platform(tonyLibs.jacksonBom))
+    add("api", platform(tonyLibs.springBootDependencies))
+    add("api", platform(tonyLibs.springCloudDependencies))
 }
