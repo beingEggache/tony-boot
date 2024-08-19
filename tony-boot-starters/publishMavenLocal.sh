053d404bf2eb91889e3fcc9c469ac358ade0e0f7
Tony <tony@91aoyou.com>
2024-08-19 09:47:59 +0800

    update:.

--- tony-boot-starters/publishMavenLocal.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-./gradlew build-script:publishJarAndSrcPublicationToMavenLocal
-
-./gradlew ktlintFormat
-./gradlew clean
-./gradlew publishPomPublicationToMavenLocal
-./gradlew publishCatalogPublicationToMavenLocal
-./gradlew publishJarAndSrcPublicationToMavenLocal --build-cache
--- tony-boot-starters/publishPrivateRepository.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-./gradlew build-script:publishJarPublicationToPrivateGradleRepository
-
-./gradlew ktlintFormat
-./gradlew clean
-./gradlew publishPomPublicationToPrivateRepository
-./gradlew publishCatalogPublicationToPrivateRepository
-./gradlew publishJarPublicationToPrivateRepository --build-cache
