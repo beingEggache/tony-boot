0d90363ce030cd3ae2ce9334057e4d934cbf566c
Tony <tony@91aoyou.com>
2024-08-22 09:32:57 +0800

    polish

--- tony-boot-starters/tony-core/src/test/kotlin/com/tony/test/core/Test.kt
+++ tony-boot-starters/tony-core/src/test/kotlin/com/tony/test/core/Test.kt
@@ -24,21 +24,7 @@
 
 package com.tony.test.core
 
-import com.tony.utils.dateOverlap
-import com.tony.utils.overlap
-import com.tony.utils.toDate
-import java.time.LocalDate
-
 fun main() {
-    val start1 = LocalDate.of(2023, 12, 4)
-    val end1 = LocalDate.of(2023, 12, 8)
-
-    val start2 = LocalDate.of(2023, 12, 3)
-    val end2 = LocalDate.of(2023, 12, 9)
-
-    println((start1 to end1).overlap(start2 to end2))
-    println((start1.toDate() to end1.toDate()).dateOverlap(start2.toDate() to end2.toDate()))
-
 }
 
 fun quickSort(list: List<Int>): List<Int> =
