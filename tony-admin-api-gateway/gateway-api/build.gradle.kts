1bac99cae39705f947189b95c3f15625bd8b1ff9
Tony <tony@91aoyou.com>
2025-06-03 13:34:45 +0800

    update: deps update

--- tony-admin-api-gateway/gateway-api/build.gradle.kts
+++ tony-admin-api-gateway/gateway-api/build.gradle.kts
@@ -4,18 +4,20 @@ apply(plugin = rootProject.tonyLibs.plugins.kotlinSpring.get().pluginId)
 apply(plugin = rootProject.tonyLibs.plugins.tonyDocker.get().pluginId)
 dependencies {
     implementation("org.springframework.cloud:spring-cloud-starter-bootstrap")
-    implementation("org.springframework.cloud:spring-cloud-starter-gateway")
+    implementation("org.springframework.cloud:spring-cloud-starter-gateway-server-webflux")
     implementation("org.springframework.cloud:spring-cloud-starter-loadbalancer")
     implementation(tonyLibs.caffeine)
     implementation("com.tencent.cloud:spring-cloud-starter-tencent-polaris-discovery")
     implementation("com.tencent.cloud:spring-cloud-starter-tencent-polaris-config")
 
-    implementation("com.tencent.cloud:spring-cloud-tencent-gateway-plugin")
+    implementation("com.tencent.cloud:spring-cloud-starter-tencent-gateway-plugin")
     implementation("com.tencent.cloud:spring-cloud-starter-tencent-metadata-transfer")
 
     // gateway 限流用
     implementation(tonyLibs.commonsPool2)
     implementation(tonyLibs.springBootStarterDataRedisReactive)
+
+    implementation("org.json:json:20250517")
 
     implementation(templateProject("core")) { isChanging = true }
     implementation(templateProject("jwt")) { isChanging = true }
